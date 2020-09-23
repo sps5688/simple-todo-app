3b46553e92bab7273db396fe74acaf1f637828f2
Steven Shaw <sshaw5688@gmail.com>
2020-09-22 21:57:50 -0400

    Updates

---
+++ precache-manifest.1d2bbb54a6d8b6337807d94b35c71a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c6f35608fa149b9539ac38d4da3e8c",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "7434d5166f582c17dc86",
+    "url": "/simple-todo-app/static/css/main.47ab3f68.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7434d5166f582c17dc86",
+    "url": "/simple-todo-app/static/js/main.fbed75f6.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
