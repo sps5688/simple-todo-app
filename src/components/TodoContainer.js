eb698152105fcbb62cd6d80f6eb36f3ecb57f102
Steven Shaw <sshaw5688@gmail.com>
2020-09-26 20:29:35 -0400

    Converting app to use Hooks

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,125 +1,187 @@
-import React from "react";
-import { v4 as uuidv4 } from "uuid";
-import axios from "axios";
+// import React from "react";
+// import { v4 as uuidv4 } from "uuid";
+// import axios from "axios";
 
-import Header from "./Header";
+// import Header from "./Header";
+// import TodosList from "./TodosList";
+// import InputTodo from "./InputTodo";
+
+// class TodoContainer extends React.Component {
+//   state = {
+//     // todos: [
+//     //   {
+//     //     id: uuidv4(),
+//     //     title: "Setup development environment",
+//     //     completed: true,
+//     //   },
+//     //   {
+//     //     id: uuidv4(),
+//     //     title: "Develop website and add content",
+//     //     completed: false,
+//     //   },
+//     //   {
+//     //     id: uuidv4(),
+//     //     title: "Deploy to live server",
+//     //     completed: false,
+//     //   },
+//     // ],
+
+//     todos: [],
+//     show: false,
+//   };
+
+//   handleChange = (id) => {
+//     this.setState({
+//       todos: this.state.todos.map((todo) => {
+//         if (todo.id === id) {
+//           todo.completed = !todo.completed;
+//         }
+
+//         return todo;
+//       }),
+//       show: !this.state.show,
+//     });
+//   };
+
+//   // delTodo = (id) => {
+//   //   this.setState({
+//   //     todos: [
+//   //       ...this.state.todos.filter((todo) => {
+//   //         return todo.id !== id;
+//   //       }),
+//   //     ],
+//   //   });
+//   // };
+
+//   delTodo = (id) => {
+//     axios
+//       .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+//       .then((reponse) =>
+//         this.setState({
+//           todos: [
+//             ...this.state.todos.filter((todo) => {
+//               return todo.id !== id;
+//             }),
+//           ],
+//         })
+//       );
+//   };
+
+//   // addTodoItem = (title) => {
+//   //   const newTodo = {
+//   //     id: uuidv4(),
+//   //     title: title,
+//   //     completed: false,
+//   //   };
+
+//   //   this.setState({
+//   //     todos: [...this.state.todos, newTodo],
+//   //   });
+//   // };
+
+//   addTodoItem = (title) => {
+//     axios
+//       .post("https://jsonplaceholder.typicode.com/todos", {
+//         title: title,
+//         completed: false,
+//       })
+//       .then((response) =>
+//         this.setState({
+//           todos: [...this.state.todos, response.data],
+//         })
+//       );
+//   };
+
+//   componentDidMount() {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/todos", {
+//         params: {
+//           _limit: 10,
+//         },
+//       })
+//       .then((response) => this.setState({ todos: response.data }));
+//   }
+
+//   render() {
+//     return (
+//       //   <div>
+//       //     <h1>Hello from Create React App</h1>
+//       //     <p>I am in a React Component!</p>
+//       //   </div>
+//       // <React.Fragment>
+//       // </React.Fragment>
+//       <div className="container">
+//         <Header headerSpan={this.state.show} />
+//         <InputTodo addTodoProps={this.addTodoItem} />
+//         <TodosList
+//           todos={this.state.todos}
+//           handleChangeProps={this.handleChange}
+//           deleteTodoProps={this.delTodo}
+//         />
+//       </div>
+//     );
+//   }
+// }
+// export default TodoContainer;
+
+import React, { useState, useEffect } from "react";
 import TodosList from "./TodosList";
+import Header from "./Header";
 import InputTodo from "./InputTodo";
 
-class TodoContainer extends React.Component {
-  state = {
-    // todos: [
-    //   {
-    //     id: uuidv4(),
-    //     title: "Setup development environment",
-    //     completed: true,
-    //   },
-    //   {
-    //     id: uuidv4(),
-    //     title: "Develop website and add content",
-    //     completed: false,
-    //   },
-    //   {
-    //     id: uuidv4(),
-    //     title: "Deploy to live server",
-    //     completed: false,
-    //   },
-    // ],
+import axios from "axios";
+import { v4 as uuid } from "uuid";
 
-    todos: [],
-    show: false,
-  };
+const TodoContainer = (props) => {
+  const [todos, setTodos] = useState([]);
+  const [show, setShow] = useState(false);
 
-  handleChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
+  const handleChange = (id) => {
+    setTodos(
+      todos.map((todo) => {
         if (todo.id === id) {
           todo.completed = !todo.completed;
         }
-
         return todo;
-      }),
-      show: !this.state.show,
-    });
-  };
-
-  // delTodo = (id) => {
-  //   this.setState({
-  //     todos: [
-  //       ...this.state.todos.filter((todo) => {
-  //         return todo.id !== id;
-  //       }),
-  //     ],
-  //   });
-  // };
-
-  delTodo = (id) => {
-    axios
-      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then((reponse) =>
-        this.setState({
-          todos: [
-            ...this.state.todos.filter((todo) => {
-              return todo.id !== id;
-            }),
-          ],
-        })
-      );
-  };
-
-  // addTodoItem = (title) => {
-  //   const newTodo = {
-  //     id: uuidv4(),
-  //     title: title,
-  //     completed: false,
-  //   };
-
-  //   this.setState({
-  //     todos: [...this.state.todos, newTodo],
-  //   });
-  // };
-
-  addTodoItem = (title) => {
-    axios
-      .post("https://jsonplaceholder.typicode.com/todos", {
-        title: title,
-        completed: false,
       })
-      .then((response) =>
-        this.setState({
-          todos: [...this.state.todos, response.data],
-        })
-      );
-  };
-
-  componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos", {
-        params: {
-          _limit: 10,
-        },
-      })
-      .then((response) => this.setState({ todos: response.data }));
-  }
-
-  render() {
-    return (
-      //   <div>
-      //     <h1>Hello from Create React App</h1>
-      //     <p>I am in a React Component!</p>
-      //   </div>
-      // <React.Fragment>
-      // </React.Fragment>
-      <div className="container">
-        <Header headerSpan={this.state.show} />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
-      </div>
     );
-  }
-}
+    setShow(!show);
+  };
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((response) => setTodos(response.data));
+  }, []);
+
+  return (
+    <div className="container">
+      <Header headerSpan={show} />
+      <InputTodo addTodoProps={addTodoItem} />
+      <TodosList
+        todos={todos}
+        handleChangeProps={handleChange}
+        deleteTodoProps={delTodo}
+      />
+    </div>
+  );
+};
+
 export default TodoContainer;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,21 +1,41 @@
+// import React from "react";
+// import TodoItem from "./TodoItem";
+
+// class TodoList extends React.Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         {this.props.todos.map((todo) => (
+//           <TodoItem
+//             key={todo.id}
+//             todo={todo}
+//             handleChangeProps={this.props.handleChangeProps}
+//             deleteTodoProps={this.props.deleteTodoProps}
+//           />
+//         ))}
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default TodoList;
+
 import React from "react";
 import TodoItem from "./TodoItem";
 
-class TodoList extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        {this.props.todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-          />
-        ))}
-      </React.Fragment>
-    );
-  }
-}
+const TodosList = (props) => {
+  return (
+    <div>
+      {props.todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default TodoList;
+export default TodosList;
